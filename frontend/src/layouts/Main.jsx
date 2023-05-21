b578980a194c74284006ccd0ea713f77121de83a
JaehoLee <chosloo2835@gmail.com>
2023-05-21 19:27:06 +0900

    add : 리액트 프로젝트 셋팅 (#5)
    1. react tailwind
    2. 컴포넌트화
    3. react-router v6

---
+++ frontend/src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Maker from "../pages/Maker";
+import BookStore from "../pages/BookStore";
+
+export default function Main() {
+  return (
+    <main className="h-full custom-min-height bg-white">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Maker" element={<Maker />} />
+        <Route exact path="/BookStore" element={<BookStore />} />
+      </Routes>
+    </main>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Main from "./layouts/Main.jsx";
+import Header from "./layouts/Header.jsx";
+import Footer from "./layouts/Footer.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <Main />
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
+);
