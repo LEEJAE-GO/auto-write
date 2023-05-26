289b31f76657c942624e678d16095de7aba23d3b
hojae-lee <chosloo2835@gmail.com>
2023-05-26 20:40:35 +0900

    refactor : footer 주소 변경

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
+          <a href="https://github.com/LEEJAE-GO/" className="hover:underline">
+            LEEJAE-GO
           </a>
           . All Rights Reserved.
         </span>
