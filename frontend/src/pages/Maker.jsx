0901da9334d9f64e716b16e9ec2f47eb87b64f55
hojae-lee <chosloo2835@gmail.com>
2023-05-26 20:40:51 +0900

    refactor : maker 여백 공통화

--- frontend/src/pages/Maker.jsx
+++ frontend/src/pages/Maker.jsx
@@ -2,9 +2,18 @@ import InputBox from "../components/InputBox";
 
 export default function Maker() {
   return (
-    <section className="relative overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-64">
+    <section className="relative overflow-hidden bg-white px-6 py-24 sm:py-16 lg:px-8 lg:max-w-7xl mx-auto">
       <div className="w-full">
         <InputBox />
+        <div className="selection:bg-fuchsia-300 selection:text-fuchsia-900 text-black py-4">
+          <p>
+            So I started to walk into the water. I won't lie to you boys, I was
+            terrified. But I pressed on, and as I made my way past the breakers
+            a strange calm came over me. I don't know if it was divine
+            intervention or the kinship of all living things but I tell you
+            Jerry at that moment, I <em>was</em> a marine biologist.
+          </p>
+        </div>
       </div>
     </section>
   );
